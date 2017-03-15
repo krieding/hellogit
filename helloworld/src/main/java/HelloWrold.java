6b5cbe52195f7cd1e3243e87621f9436f0d49e08
krieding@163.com <zzx911102>
2017-03-15 15:24:08 +0800

    helloworld测试工程

---
+++ helloworld/src/main/java/HelloWrold.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWorld
+ * @author zhangzhongxiang
+ * @create 2017-03-15 15:22
+ **/
+public class HelloWrold {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
